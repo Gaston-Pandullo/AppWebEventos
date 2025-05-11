4623d234d37e0607d473109156135b6ed4d5cda0
Gaston Pandullo <gpandullo3@gmail.com>
2025-05-11 18:35:06 -0300

    Agregar archivos de proyecto.

---
+++ AppWebEventos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppWebEventos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
